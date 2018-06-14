ed4680ff1f9ee52c3c417c66bf604d65cf372707
Chjyb <1696917651@qq.com>
2018-06-14 10:00:00 +0800

    添加项目文件。

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult login()
+        {
+            return View();
+        }
+    }
+}
